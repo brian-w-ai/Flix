ba3ba2937b43b31c80b08c753be4a633527b677f
Brian Ai <brianai@gmail.com>
2021-10-15 23:50:41 -0700

    Added movieGridViewer and fixed auto resizing bug

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Brian Ai on 10/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
